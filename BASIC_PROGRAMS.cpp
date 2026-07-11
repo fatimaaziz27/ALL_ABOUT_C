48480adc42bfdcf13b3596102780cfebc2aa4ae2
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-11 19:04:10 +0500

    Update BASIC_PROGRAMS.cpp

--- BASIC_PROGRAMS.cpp
+++ BASIC_PROGRAMS.cpp
@@ -28,7 +28,15 @@ int main(){
 
 
 
-
+# include<stdio.h>
+int main(){
+    int i,table;
+    printf("Type any table number: ");
+    scanf("%d",&table);
+    for (i=1; i<=10; i++){
+        printf("%d X %d = %d \n",table,i,table*i);
+    }
+}
 
 
 
