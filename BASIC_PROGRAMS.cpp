4c2219882bab98080d9f02eb4d129275e6312b7a
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-11 15:04:56 +0500

    Update BASIC_PROGRAMS.cpp

--- BASIC_PROGRAMS.cpp
+++ BASIC_PROGRAMS.cpp
@@ -1,1 +1,10 @@
-
+# include <stdio.h>
+// # include <conio.h>
+int main(){
+    int a,b=10,c=15;
+    for(a=6; a<=8; a++){
+        b=b+2;
+        c=c+3;
+        printf("%d\t%d\t%d\t\n",a,b,c);
+    }
+}
