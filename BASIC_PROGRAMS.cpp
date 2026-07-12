4820b49de4f5c63a5b2e5d92c8a63a088cb3aada
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-13 00:30:17 +0500

    Update BASIC_PROGRAMS.cpp

--- BASIC_PROGRAMS.cpp
+++ BASIC_PROGRAMS.cpp
@@ -10,6 +10,14 @@ int main(){
 }
 
 // Variable Increment Program
+# include<stdio.h>
+int main(){
+    int a,b=10,c=15;
+    for(a=6; a<=8; a++){
+        b=b+2; c=c+3;
+        printf("%d \t %d \t %d \t \n",a,b,c);
+    }
+}
 
 // factorial
 # include<stdio.h>
