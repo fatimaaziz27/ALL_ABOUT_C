af3d7f822e0e79973f1ce36e2393d24ae918b6d3
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-07-25 14:32:54 +0500

    Update BASIC_PROGRAMS.cpp

--- BASIC_PROGRAMS.cpp
+++ BASIC_PROGRAMS.cpp
@@ -1,3 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Type value of num1: ");
+    scanf("%d",&a);
+    printf("Type value of num2: ");
+    scanf("%d",&b);
+    c = a+b;
+    printf("num1 + num2 = %d",c);
+
+}
+
 // loop_variables
 # include <stdio.h>
 int main(){
