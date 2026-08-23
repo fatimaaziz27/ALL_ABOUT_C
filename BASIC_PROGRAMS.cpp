72fdcb0f806e148c0f234feeebc0d39c6c47568b
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-08-23 17:14:56 +0500

    Update BASIC_PROGRAMS.cpp

--- BASIC_PROGRAMS.cpp
+++ BASIC_PROGRAMS.cpp
@@ -13,23 +13,16 @@ int main()
 }
 
 // Subtraction
-
 #include <stdio.h>
-
 int main()
 {
     int a, b, c;
-
     printf("Type value of num1: ");
     scanf("%d", &a);
-
     printf("Type value of num2: ");
     scanf("%d", &b);
-
     c = a - b;
-
     printf("num1 - num2 = %d", c);
-
     return 0;
 }
 
