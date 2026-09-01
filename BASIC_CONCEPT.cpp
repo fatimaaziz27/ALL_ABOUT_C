b08a04a2e7a6f399a06e05650c979ae5bbcae8eb
fatimaaziz27 <fatimaaziz250907@gmail.com>
2026-09-01 08:18:50 +0500

    Update BASIC_CONCEPT.cpp

--- BASIC_CONCEPT.cpp
+++ BASIC_CONCEPT.cpp
@@ -7,4 +7,51 @@
   // main code here <---->
 }
 
+// Print Statement ----->
+// Syntax
+printf( "write here..." );
+// Code:
+printf("Hello");
 
+// Types Of Comments ------->
+// single line comments
+// comment here ......
+// multi line comments
+/*   comment here ......   */
+
+// Data Types 
+// Name           Type               Keyword 
+  bool           true/false           ---
+  int            123456577            %d
+  char/string    letter/word        %c / %s
+  float          3.14                 %f
+
+// Operators ------>
+
+// Arithemetic Assignment Operators
+|   +=   |   ==   |   *=   |   /=   |
+
+// Increment / Decrement 
+|   ++   |   --   |    
+
+// Relational Operators 
+|   >   |   ==   |   <   |   <=   |   >=   |   !   |
+
+// Logical Operators
+|   &&   |   AND   |
+|   ||   |   OR   |
+|   !   |   NOT   |
+
+Assignig
+
+
+
+
+
+
+
+
+
+
+    
+    
